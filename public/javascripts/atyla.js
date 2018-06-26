b59878cf308471b7a74f33011af9cce83bdfaff8
maisonbleue <vincent.trastour@lecollectionist.com>
2018-06-26 22:41:39 +0200

    MASTER update button

--- public/javascripts/atyla.js
+++ public/javascripts/atyla.js
@@ -1,32 +1,34 @@
 var btn = document.createElement('BUTTON');
 
-btn.style.height = '50px';
-btn.style.width = '300px';
+btn.style.height = '100%';
+btn.style.width = '100%';
 btn.style.color = '#fff';
 btn.style.border = 'none';
 btn.style.outline = 'none';
-btn.style.backgroundColor = '#CD5542';
+btn.style.backgroundColor = '#4CA763';
 btn.style.borderRadius = '12px';
 
 var spanRight = document.createElement('SPAN');
 var spanRightText = document.createTextNode("Achetez Maintenant");
-spanRight.style.borderLeft = '2px solid #fff';
-spanRight.style.paddingLeft = '15px';
-spanRight.style.fontSize = '20px';
 spanRight.appendChild(spanRightText);
+
+var spanCenter = document.createElement('SPAN');
+spanCenter.style.height = '80%';
+spanCenter.style.border = '1px solid #fff';
 
 var spanLeft = document.createElement('SPAN');
 var spanLeftText = document.createTextNode("atyla");
-spanLeft.style.marginRight = '15px';
-spanLeft.style.fontSize = '20px';
-spanLeft.style.marginLeft = '15px';
 spanLeft.style.fontWeight = '600';
 spanLeft.appendChild(spanLeftText);
 
 var spanGlobal = document.createElement("SPAN");
+spanGlobal.style.margin = '0 5px 0 5px';
+spanGlobal.style.height = '100%'
 spanGlobal.style.display = 'flex';
 spanGlobal.style.alignItems = 'center';
+spanGlobal.style.justifyContent = 'space-between';
 spanGlobal.appendChild(spanLeft);
+spanGlobal.appendChild(spanCenter);
 spanGlobal.appendChild(spanRight);
 btn.appendChild(spanGlobal);
 document.getElementById('atylaDiv').appendChild(btn);
